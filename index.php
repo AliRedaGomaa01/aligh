a41dd55195c3d5e70cc83c1580c6dbad1e1b1d31
Ali Reda <alyredagomaa@gmail.com>
2024-02-20 13:55:42 +0200

    edit website icon

--- index.php
+++ index.php
@@ -12,6 +12,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="./images/<?= $isEn ? 'logoEn' : 'logoAr' ?>.png">
     <!-- Tailwind -->
     <script src="./tailwind.js"></script>
     <script>
