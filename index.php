04dbaa3598a853973ea45e632a0449da87b9c778
Ali Reda <alyredagomaa@gmail.com>
2025-11-18 16:01:43 +0200

    fix styles

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ $isEn = $lang == 'en';
   <main class="container mx-auto p-5 flex-1">
 
     <section
-      class="grid grid-cols-1 gap-10 mx-auto shadow-[-3px_-3px_3px_0px_rgb(0,0,0,0.5),3px_3px_3px_0px_rgb(0,0,0,0.5)] p-10 leading-8 rounded-xl max-w-[800px] ">
+      class="grid grid-cols-1 gap-10 mx-auto shadow-[-3px_-3px_3px_0px_rgb(0,0,0,0.5),3px_3px_3px_0px_rgb(0,0,0,0.5)] p-5 sm:p-10 leading-8 rounded-xl max-w-[800px] ">
 
       <?php include "sections/section1.php" ?>
 
