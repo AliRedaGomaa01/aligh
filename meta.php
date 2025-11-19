4fdc60f2d2269e67e10ca091b6e214e321861ba6
Ali Reda <alyredagomaa@gmail.com>
2025-11-19 06:34:36 +0200

    update meta tags

--- meta.php
+++ meta.php
@@ -1,32 +1,27 @@
 <title>Ali Hussein - Full-Stack Developer</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta name="description" content="Ali Hussein, a passionate full-stack web and mobile app developer.">
+<meta name="description" content="Ali Hussein, a passionate full-stack web developer.">
 <meta name="keywords"
-  content="Ali Hussein, Full-Stack Developer, Web Development, Mobile Development, alyhsn.com, alyhsn, Aly Hussein, Developer">
+  content="ali, aly, hussein, alyhsn.com, alyhsn, developer, development, web, website, software, programmer, coding, programming, end, stack, software engineer, IT, technology">
 <meta name="robots" content="index, follow">
 
 <!-- Open Graph -->
 <meta property="og:title" content="Ali Hussein - Full-Stack Developer">
-<meta property="og:description" content="Ali Hussein, a passionate full-stack web and mobile app developer.">
-<meta property="og:image" content="https://alyhsn.com/logoEn.png">
+<meta property="og:description" content="Ali Hussein, a passionate full-stack web app developer.">
+<meta property="og:image" content="https://alyhsn.com/images/img-new.jpg">
 <meta property="og:url" content="https://alyhsn.com">
-<meta property="og:url" content="https://alyhsn.com" />
 <meta property="og:site_name" content="Ali Hussein" />
 <meta property="og:locale" content="en-US" />
 <meta property="og:type" content="website" />
-
-<meta name="googlebot" content="index, follow, max-video-preview:-1, max-image-preview:large, max-snippet:-1" />
-<meta name="google-site-verification" content="p9BgvFqUG9DRMza4vhNQXgC9g5Tt2L5j2VTUXwupzSs" />
-<!-- <meta name="yandex-verification" content="14d2e73487fa6c71" /> -->
 
 <!-- Twitter Cards -->
 <meta name="twitter:card" content="summary_large_image">
 <meta name="twitter:title" content="Ali Hussein - Full-Stack Developer">
 <meta name="twitter:description" content="Ali Hussein, a passionate full-stack web and mobile app developer.">
-<meta name="twitter:image" content="https://alyhsn.com/logoEn.png">
+<meta name="twitter:image" content="https://alyhsn.com/images/img-new.jpg">
 
 <!-- Canonical -->
 <link rel="canonical" href="https://alyhsn.com">
-<link rel="icon" type="image/png" href="./images/<?= $isEn ? 'logoEn' : 'logoAr' ?>.png">
-<link rel="apple-touch-icon" href="./images/<?= $isEn ? 'logoEn' : 'logoAr' ?>.png">
+<link rel="icon" type="image/png" href="/images/img-new.jpg">
+<link rel="apple-touch-icon" href="/images/img-new.jpg">
