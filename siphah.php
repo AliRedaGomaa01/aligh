c12ec804aff7bd692b834876c0b7b4a5e5a219fa
Ali Reda <alyredagomaa@gmail.com>
2025-11-28 05:06:44 +0200

    refactor siphah

--- siphah.php
+++ siphah.php
@@ -1,8 +1,11 @@
 <?php include "./partials/before-main.php" ?>
 
-  <!-- Main -->
-  <main class="container mx-auto p-5 grid place-items-center flex-1" onclick="increaseCounter()">
-    <div id="counter" class="text-4xl">0</div>
-  </main>
+<!-- Main -->
+<main class="container mx-auto p-5 grid align-items-stretch justify-items-stretch flex-1">
+  <div id="counter" class="text-5xl bg-indigo-50 rounded-xl shadow-lg grid place-items-center select-none cursor-pointer"
+    onclick="this.innerText = +this.innerText +1">
+    0
+  </div>
+</main>
 
 <?php include "./partials/after-main.php" ?>
