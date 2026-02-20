cea13d75cf6c94cc381c494f21f8cd6d1b3a33d9
Ali Reda <alyredagomaa@gmail.com>
2026-02-20 09:43:42 +0200

    increase max width of main  section

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <main class="container mx-auto p-5 flex-1 flex flex-col items-center justify-center gap-10">
   <?php 
 
-  $parentSectionClasses = "grid grid-cols-1 gap-5 md:gap-10 mx-auto shadow-[-3px_-3px_3px_0px_rgb(0,0,0,0.5),3px_3px_3px_0px_rgb(0,0,0,0.5)] p-5 sm:p-10 leading-8 rounded-xl w-full max-w-[800px] ";
+  $parentSectionClasses = "grid grid-cols-1 gap-5 md:gap-10 mx-auto shadow-[-3px_-3px_3px_0px_rgb(0,0,0,0.5),3px_3px_3px_0px_rgb(0,0,0,0.5)] p-5 sm:p-10 leading-8 rounded-xl w-full max-w-[1000px] ";
 
   $sectionTitle = " font-bold text-center container bg-main text-white text-2xl py-4  px-2 sm:text-3xl sm:py-6  sm:px-4 mx-auto rounded-lg" ?>
 
