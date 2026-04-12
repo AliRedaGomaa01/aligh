c895de34caffe085588714483b46f6d3beb61a2b
Ali Reda <alyredagomaa@gmail.com>
2026-04-12 12:55:50 +0200

    update azkar spaces

--- siphah.php
+++ siphah.php
@@ -4,23 +4,29 @@
 <main class="container flex-1 mx-auto  p-5 ">
   <section class="container mx-auto  grid align-items-stretch justify-items-stretch min-h-screen relative">
     <div
-      class="text:2xl md:text-4xl lg:text-5xl bg-gray-900 text-gray-200 rounded-xl shadow-lg grid place-items-center select-none cursor-pointer z-50 p-5 text-center"
+      class="text:xl md:text-2xl lg:text-3xl bg-gray-900 text-gray-200 rounded-xl shadow-lg grid place-items-center select-none cursor-pointer z-50 p-5 text-center"
       dir="rtl" onclick="add(+1)">
       أعوذ بالله من كل شر - وأسأله من كل خير ،،
-      
-      لا إله إلا الله وحده لا شريك له له الملك وله الحمد وهو على كل شيء قدير -
-      أقررت له بظلمي وتقصيري وضعفي وجهلي ،، 
-      
-      واستغفر الله وأتوب إليه - وسبحان الله وبحمده ،، 
-      
-      والحمد لله - والله أكبر ،،
-      وأفوض أمري إلى الله - ولا حول ولا قوة إلا بالله ،، 
-      
-      وإنا لله وإنا إليه راجعون - وصلى الله وسلم وبارك على عبده ونبيه محمد ،،
-
-      عدد كل شيء
       <br>
-      <span id="counter">0</span>
+      <br>
+      لا إله إلا الله وحده لا شريك له له الملك وله الحمد وهو على كل شيء قدير -
+      أقررت له بظلمي وتقصيري وضعفي وجهلي ،،
+      <br>
+      <br>
+      واستغفر الله وأتوب إليه - وسبحان الله وبحمده ،،
+      <br>
+      <br>
+      والحمد لله - والله أكبر ،،
+      <br>
+      <br>
+      وأفوض أمري إلى الله - ولا حول ولا قوة إلا بالله ،،
+      <br>
+      <br>
+      وإنا لله وإنا إليه راجعون - وصلى الله وسلم وبارك على عبده ونبيه محمد ،،
+      <br>
+      <br>
+      عدد كل شيء
+      <span id="counter" class="mt-10 block">0</span>
     </div>
   </section>
 
